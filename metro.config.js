bb5b43899883661d7f62bdef3d974720ccd555cd
Northern Obsidian <obsidian.north.corp@gmail.com>
2026-08-04 10:36:53 +0300

    js bundle fix build error

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.alias = {
-  '@': path.resolve(__dirname, 'src'),
-  '@assets': path.resolve(__dirname, 'assets'),
-};
 
 module.exports = config;
